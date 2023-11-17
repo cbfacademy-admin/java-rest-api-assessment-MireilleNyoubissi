dbb65e1c61ec4cfe653eee91624d9b2929ea1738
MireilleNyoubissi <nmireillemat@yahoo.fr>
2023-11-17 20:23:24 +0000

    Defining the readFile method in PersonDao and implementing the same method in UserDataAccess. create a dbUsersFile.json in resources folder.

--- src/main/java/com/cbfacademy/apiassessment/dao/PersonDAO.java
+++ src/main/java/com/cbfacademy/apiassessment/dao/PersonDAO.java
@@ -1,10 +1,17 @@
 package com.cbfacademy.apiassessment.dao;
 
+import java.util.List;
+import com.cbfacademy.apiassessment.model.User;
+
 
 /**
  * Support all types of data access that a person (Ex: user, admin) using our blog app can have with our JSON file. 
  */
 public interface PersonDAO {
 
-    
+    /**
+     * Read JSON file and return a list of users.
+     */
+    List<User> readFile();
+
 }
--- src/main/java/com/cbfacademy/apiassessment/dao/UserDataAccess.java
+++ src/main/java/com/cbfacademy/apiassessment/dao/UserDataAccess.java
@@ -1,9 +1,45 @@
 package com.cbfacademy.apiassessment.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.util.ResourceUtils;
+
+import com.cbfacademy.apiassessment.model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 /**
  * users data access.
  */
 public class UserDataAccess implements PersonDAO {
+
+    File file = new File("src/main/resources/dbUsersFile");
+
+    @Override
+    public List<User> readFile() {
+    List<User> dbUser = new ArrayList<>();
+    
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        
+        Type listType = new TypeToken<List<User>>() {
+        }.getType();
+        
+        try (JsonReader reader = new JsonReader(new FileReader(file.getAbsolutePath()));) {
+            System.out.println(reader.toString());
+            dbUser = gson.fromJson(reader, listType);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(dbUser);
+        return dbUser;
+    }
+
     
 }
