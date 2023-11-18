33fc52f4922132d33e59b293284a643054f04103
MireilleNyoubissi <nmireillemat@yahoo.fr>
2023-11-18 14:49:40 +0000

    created the the PutMapping endpoint in the App.ja controller and included the RequestBody and the PathVariable annotations

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +61,10 @@ public class App {
         userService.deleteUserById(userId);
 	}
 
-
+	//Update the user with the specify id
+	@PutMapping(path = "{id}")
+    public void updateUserById(@RequestBody User user, @PathVariable("id") UUID userId) {
+        userService.updateUserById(user, userId);
+    }
 
 }
