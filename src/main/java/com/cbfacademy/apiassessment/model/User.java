b00e0dd045e2e1aee5a0435b6594fc3adb930db1
MireilleNyoubissi <nmireillemat@yahoo.fr>
2023-11-18 20:03:25 +0000

    added validation requirements for the user payload

--- src/main/java/com/cbfacademy/apiassessment/model/User.java
+++ src/main/java/com/cbfacademy/apiassessment/model/User.java
@@ -3,12 +3,21 @@ package com.cbfacademy.apiassessment.model;
 import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
 public class User implements Serializable{
     
     //adding fields
     private UUID userId;
+    @Size(min = 2, message = "Name should have at least two characters")
     private String username;
+    @Email(message = "Please provide a valid email address ex: username@domain.com")
     private String email;
+    @Min(value = 18, message = "Age should not be less than 18")
+    @Max(value = 100, message = "Age should not be greater than 100")
     private int age;
 
     //adding parameterized constructor
